4672fe7a00c331f801b1e7c3c39388cff7aad6d4
Shuran <jian0066@gmail.com>
2015-06-15 11:14:46 +0800

    Deleted unnecessary logicTaskManager.cs

--- meloveShared/meloveShared/BL/Loaders/UtilitiesThreadLoader.cs
+++ meloveShared/meloveShared/BL/Loaders/UtilitiesThreadLoader.cs
@@ -5,54 +5,33 @@ using System.Threading;
 
 namespace meloveShared.BL
 {
-	/*
-	public static class UtilitiesThreadLoader
+	public class UtilitiesThreadLoader
 	{
-		public static UtilitiesThreadTaskScheduler mInstance { get; private set;}
-
-		public static void Init()
-		{
-			mInstance = new UtilitiesThreadTaskScheduler ();
-		}
+		//public static UtilitiesThreadTaskScheduler mWebThreadTaskScheduler;
+		//public static UtilitiesThreadTaskScheduler mFileThreadTaskScheduler;
 	}
-		
+
+	/*
 	public sealed class UtilitiesThreadTaskScheduler : TaskScheduler, IDisposable
 	{
-		private BlockingCollection<Task> mWebTasks = new BlockingCollection<Task>();
-		private BlockingCollection<Task> mFileTasks = new BlockingCollection<Task>();
+		private BlockingCollection<Task> mTasks = new BlockingCollection<Task>();
 
-		private Thread mWebThread;
-		private Thread mFileThread;
+		private Thread mThread;
 
 		public UtilitiesThreadTaskScheduler()
 		{
-			//Web Thread
-			mWebThread = new Thread 
+			mThread = new Thread 
 			(
 				() =>
 				{
-						foreach (var pTask in mWebTasks.GetConsumingEnumerable())
+						foreach (var pTask in mTasks.GetConsumingEnumerable())
 						{
 							TryExecuteTask(pTask);
 						}
 				}
 			);
-			mWebThread.IsBackground = true;
-			mWebThread.Start ();
-
-			//File Thread
-			mFileThread = new Thread 
-			(
-				() =>
-				{
-						foreach (var pTask in mFileTasks.GetConsumingEnumerable())
-						{
-							TryExecuteTask(pTask);
-						}
-				}
-			);
-			mFileThread.IsBackground = true;
-			mFileThread.Start ();
+			mThread.IsBackground = true;
+			mThread.Start ();
 		}
 
 		protected override void QueueTask(Task pTask)
